d94e2d02f774ec930eda664f5c4acef1387d44ff
ThomasMulders <0341091@student.rocvantwente.nl>
2022-05-12 11:51:14 +0200

    Image files toegevoegd aan test branch. Voor sql query maken

--- import.sql
+++ import.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE `notch`;
 
 USE `notch`;
+/* Table Users */
 
 CREATE TABLE `users` (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -10,3 +11,18 @@ CREATE TABLE `users` (
     passwords varchar(255) NOT NULL,
     rank integer(10) NOT NULL
 );
+
+/* Table Products */
+
+CREATE TABLE `products` (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    productname VARCHAR(255) NOT NULL,
+    price FLOAT(10, 2) NOT NULL,
+    summary VARCHAR(255) NOT NULL,
+    pictures VARCHAR(255) NOT NULL,
+    maincategorie VARCHAR(255) NOT NULL,
+    subcategorie VARCHAR(255) NOT NULL
+);
+
+INSERT INTO `products` (`productname`, `price`, `summary`, `maincategorie`, `subcategorie`) VALUES
+()
