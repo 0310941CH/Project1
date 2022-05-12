7a69f30c7f7756444c7ea8d118c63c5f26f9d07a
ThomasMulders <0341091@student.rocvantwente.nl>
2022-05-12 08:57:29 +0200

    Buggfix voor register.php 12/5/2022

--- register.php
+++ register.php
@@ -12,6 +12,7 @@ include_once('connection.php');
 </head>
 
 <body>
+    <!-- HTML Gedeelte: Moet nog gestyled worden! -->
     <form action="" method="POST">
         <input type="text" name="voornaam" placeholder="Voornaam">
         <input type="text" name="achternaam" placeholder="Achternaam">
@@ -21,21 +22,29 @@ include_once('connection.php');
     </form>
 
     <?php
+    // Checked met eerste if of submit button ingedrukt is.
     if (isset($_POST['submitRegister'])) {
-        $voornaam = $_POST['voornaam'];
-        $achternaam = $_POST['achternaam'];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $hash = password_hash($password, PASSWORD_DEFAULT);
+        // Checked of de input velden ingevuld zijn zo ja gaat die door het naar de database pushen.
+        if ($_POST['voornaam'] != "" && $_POST['achternaam'] != "" && $_POST['username'] != "" && $_POST['password'] != "") {
+            $voornaam = $_POST['voornaam'];
+            $achternaam = $_POST['achternaam'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            //Password Encryption
+            $hash = password_hash($password, PASSWORD_DEFAULT);
 
-        $query = 'INSERT INTO users (voornaam, achternaam, username, passwords) VALUES (:vname, :aname, :uname, :pswrd)';
-        $values = [':vname' => $voornaam, ':aname' => $achternaam, ':uname' => $username, ':pswrd' => $hash];
+            // PDO Gedeelte
+            $query = 'INSERT INTO users (voornaam, achternaam, username, passwords) VALUES (:vname, :aname, :uname, :pswrd)';
+            $values = [':vname' => $voornaam, ':aname' => $achternaam, ':uname' => $username, ':pswrd' => $hash];
 
 
-        $execute = $pdo->prepare($query);
-        $execute->execute($values);
-        header('Location: login.php');
-        exit();
+            $execute = $pdo->prepare($query);
+            $execute->execute($values);
+            header('Location: login.php');
+            exit();
+        } else {
+            echo "<h1> Invalid Input </h1>";
+        }
     }
 
 
