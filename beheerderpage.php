fd8b9608cfd2b9fca496cee350598c6e3d614dcc
ThomasMulders <0341091@student.rocvantwente.nl>
2022-05-16 12:38:57 +0200

    beheerder pages toegevoegd Begin

---
+++ beheerderpage.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include_once("config/connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Main Page</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="js/searchbar.js"></script>
+</head>
+
+<body>
+
+</body>
+</html>
