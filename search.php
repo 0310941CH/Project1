76b61c74ae2f52d89d694c15de3e4ad7a1ccaa8d
Carlito Hendriks <0310941@student.rocvantwente.nl>
2022-05-17 20:15:30 +0200

    Sorteren op de search pagina en output als product niet bestaat

--- search.php
+++ search.php
@@ -46,35 +46,106 @@ include_once("config/connection.php");
         </div>
 
     </nav>
-    <!-- Search output -->
     <?php
     if (isset($_GET["searchinput"])) {
+        // Variables nodig voor werking
         $search = $_GET["searchinput"];
-        $stmt  = $pdo->prepare("SELECT * FROM products WHERE productname LIKE :pname OR maincategorie LIKE :mcategorie OR subcategorie LIKE :scategorie");
-        $stmt->execute(['pname' => "%$search%", 'mcategorie' => "%$search%", 'scategorie' => "%$search%"]);
-        $data = $stmt->fetchAll();
-
-      echo "<div class='productplace'>"  ;
-        foreach ($data as $product) {
-            echo "<div class='productinner'>";
-            echo "<div class='imagesize'>";
-            echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>". "<br>" ;
-            echo "</div>" . "<br>";
-            echo "<div class='innerinfo'>";
-            echo $product["productname"] . "<br>";
-            echo "</div> " . "<br>";
-            echo "<div class='pricebutton'>";
-            echo "€ " . $product["price"];
-            echo "<br>";  
-            echo "<br>";  
-            echo "<input type='submit' class='shopbutton' value='Add to cart'>";
-            echo "</div>";
-            echo "</div>";
-        }
+    if (isset($_GET["sort"])) {
+    
+    if (strpos($_GET["sort"], 'nAsc') !== false) { // als $_GET["sort"] nAsc bevat dan..
+        $columnName = "productname";
+        $sortBy = "asc";
+    } else if (strpos($_GET["sort"], 'nDesc') !== false) {
+        $columnName = "productname";
+        $sortBy = "desc";
+    } else if (strpos($_GET["sort"], 'pDesc') !== false) {
+        $columnName = "price";
+        $sortBy = "desc";
+    } else if (strpos($_GET["sort"], 'pAsc') !== false) {
+        $columnName = "price";
+        $sortBy = "asc";
     }
-  echo "</div>";
-  
-  ?>
+    // sorted search output
+    $_SESSION["search"] = $_GET["searchinput"];
+    $search = $_SESSION["search"];
+    $stmt  = $pdo->prepare("SELECT * FROM products WHERE productname LIKE :pname OR maincategorie LIKE :mcategorie OR subcategorie LIKE :scategorie ORDER BY $columnName $sortBy");
+    $stmt->execute(['pname' => "%$search%", 'mcategorie' => "%$search%", 'scategorie' => "%$search%"]);
+    $data = $stmt->fetchAll();
+
+    // search order manieren
+    echo '<div class="sortproducts">';
+    echo count($data) .' results for "'. $search. '"';
+    if (count($data) != 0) {
+    echo '<a href="search.php?searchinput=' . $search . '&sort=nAsc">Name Ascending</a>
+    <a href="search.php?searchinput=' . $search . '&sort=nDesc">Name Descending</a>
+    <a href="search.php?searchinput=' . $search . '&sort=pAsc">Price Ascending</a>
+    <a href="search.php?searchinput=' . $search . '&sort=pDesc">Price Descending</a>';
+    }
+    echo '</div>';
+
+    echo "<div class='productplace'>";
+    foreach ($data as $product) {
+        echo "<div class='productinner'>";
+        echo "<div class='imagesize'>";
+        echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>". "<br>" ;
+        echo "</div>" . "<br>";
+        echo "<div class='innerinfo'>";
+        echo $product["productname"] . "<br>";
+        echo "</div> " . "<br>";
+        echo "<div class='pricebutton'>";
+        echo "€ " . $product["price"];
+        echo "<br>";  
+        echo "<br>";  
+        echo "<input type='submit' class='shopbutton' value='Add to cart'>";
+        echo "</div>";
+        echo "</div>";
+    }
+    } else {
+    // Search output zonder toegepast filter
+    $_SESSION["search"] = $_GET["searchinput"];
+    $search = $_SESSION["search"];
+    $stmt  = $pdo->prepare("SELECT * FROM products WHERE productname LIKE :pname OR maincategorie LIKE :mcategorie OR subcategorie LIKE :scategorie");
+    $stmt->execute(['pname' => "%$search%", 'mcategorie' => "%$search%", 'scategorie' => "%$search%"]);
+    $data = $stmt->fetchAll();
+
+    // search order manieren
+    echo '<div class="sortproducts">';
+    echo count($data) .' results for "'. $search. '"';
+    if (count($data) != 0) {
+    echo '<a href="search.php?searchinput=' . $search . '&sort=nAsc">Name Ascending</a>
+    <a href="search.php?searchinput=' . $search . '&sort=nDesc">Name Descending</a>
+    <a href="search.php?searchinput=' . $search . '&sort=pAsc">Price Ascending</a>
+    <a href="search.php?searchinput=' . $search . '&sort=pDesc">Price Descending</a>';
+    }
+    echo '</div>';
+
+    echo "<div class='productplace'>";
+    foreach ($data as $product) {
+        echo "<div class='productinner'>";
+        echo "<div class='imagesize'>";
+        echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>". "<br>" ;
+        echo "</div>" . "<br>";
+        echo "<div class='innerinfo'>";
+        echo $product["productname"] . "<br>";
+        echo "</div> " . "<br>";
+        echo "<div class='pricebutton'>";
+        echo "€ " . $product["price"];
+        echo "<br>";  
+        echo "<br>";  
+        echo "<input type='submit' class='shopbutton' value='Add to cart'>";
+        echo "</div>";
+        echo "</div>";
+    }
+}
+    }
+    echo "</div>";
+
+    if (count($data) == 0) {
+        echo '<div class="noitems">';
+        echo '<h1> Oops! Looks like we dont have any results for "' . $search . '"</h1><br>';
+        echo 'Check your spelling or use more general terms.</div>';
+    }
+?>
 
 </body>
 
