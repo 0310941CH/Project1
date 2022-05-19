dbd990e617db0b888a7d1a922558906400391f19
Carlito Hendriks <0310941@student.rocvantwente.nl>
2022-05-19 14:05:53 +0200

    Alle product pagina's, nog niet afgewerkt

---
+++ gpu.php
@@ -0,0 +1,139 @@
+<?php
+session_start();
+include_once("config/connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GPU</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="js/searchbar.js"></script>
+</head>
+
+<body>
+    <!-- Nav bar -->
+    <nav>
+        <a href="index.php"><img src="images/notchLogo.png" class="notchlogo"></a>
+        <div class="middlenav">
+            <div class="buttons">
+                <p class="selectedtab tabs">COMPONENTS <img src="images/caret-down-solidblack.png" alt="" class="dropdownicon"></p>
+            </div>
+
+
+            <p class="tabs">PERIPHERALS <img src="images/caret-down-solidwhite.png" alt="" class="dropdownicon"></p>
+            <p class="tabs">PC'S AND LAPTOPS <img src="images/caret-down-solidwhite.png" alt="" class="dropdownicon"></p>
+        </div>
+        <div class="endnav">
+            <img class="xicon" src="images/xicon.png" alt="xicon" id="xicon" onclick="searchbarhider()">
+            <div id="divdiv">
+
+                <form class="search2" id="search2" onclick="searchbarshower()" method="get" action="search.php">
+
+                    <input type="text" class="searchinput" id="search" name="searchinput">
+                    <img src="images/blacksearch.png">
+                    <input type="submit" id="submitbutton">
+
+                </form>
+            </div>
+        </div>
+        <img src="images/dots.png" alt="options" class="icon">
+        <img src="images/register.png" class="icon">
+        <img src="images/shoppingCard.png" class="icon">
+        </div>
+
+    </nav>
+    <?php
+    if (isset($_GET["sort"])) {
+    
+    if (strpos($_GET["sort"], 'nAsc') !== false) { // als $_GET["sort"] nAsc bevat dan..
+        $columnName = "productname";
+        $sortBy = "asc";
+    } else if (strpos($_GET["sort"], 'nDesc') !== false) {
+        $columnName = "productname";
+        $sortBy = "desc";
+    } else if (strpos($_GET["sort"], 'pDesc') !== false) {
+        $columnName = "price";
+        $sortBy = "desc";
+    } else if (strpos($_GET["sort"], 'pAsc') !== false) {
+        $columnName = "price";
+        $sortBy = "asc";
+    }
+
+    // Output met toegepast filter
+    $stmt  = $pdo->prepare("SELECT * FROM products WHERE subcategorie = :scategorie ORDER BY $columnName $sortBy");
+    $stmt->execute(['scategorie' => "gpu"]);
+    $data = $stmt->fetchAll();
+
+    // search order manieren
+    echo '<div class="sortproducts">';
+    if (count($data) != 0) {
+        echo '<a href="gpu.php?sort=nAsc"><button class ="orderbutton">ABC▲</button></a>
+        <a href="gpu.php?sort=nDesc"><button class ="orderbutton">ABC▼</button></a>
+        <a href="gpu.php?sort=pAsc"><button class ="orderbutton">€▲</button></a>
+        <a href="gpu.php?sort=pDesc"><button class ="orderbutton">€▼</button></a>';
+        }
+        echo '</div>';
+
+    echo "<div class='productplace'>";
+    foreach ($data as $product) {
+        echo "<div class='productinner'>";
+        echo "<div class='imagesize'>";
+        echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>". "<br>" ;
+        echo "</div>" . "<br>";
+        echo "<div class='innerinfo'>";
+        echo $product["productname"] . "<br>";
+        echo "</div> " . "<br>";
+        echo "<div class='pricebutton'>";
+        echo "€ " . $product["price"];
+        echo "<br>";
+        echo "<a href='product.php?pid=" . $product["id"] . "&category=gpu'>Details werkt nog niet</a>";
+        echo "<br>";  
+        echo "<input type='submit' class='shopbutton' value='Add to cart'>";
+        echo "</div>";
+        echo "</div>";
+    }
+    } else {
+    // output zonder toegepast filter
+    $stmt  = $pdo->prepare("SELECT * FROM products WHERE subcategorie = :scategorie");
+    $stmt->execute(['scategorie' => "gpu"]);
+    $data = $stmt->fetchAll();
+
+    // search order manieren
+    echo '<div class="sortproducts">';
+    if (count($data) != 0) {
+        echo '<a href="gpu.php?sort=nAsc"><button class ="orderbutton">ABC▲</button></a>
+        <a href="gpu.php?sort=nDesc"><button class ="orderbutton">ABC▼</button></a>
+        <a href="gpu.php?sort=pAsc"><button class ="orderbutton">€▲</button></a>
+        <a href="gpu.php?sort=pDesc"><button class ="orderbutton">€▼</button></a>';
+        }
+        echo '</div>';
+
+    echo "<div class='productplace'>";
+    foreach ($data as $product) {
+        echo "<div class='productinner'>";
+        echo "<div class='imagesize'>";
+        echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>". "<br>" ;
+        echo "</div>" . "<br>";
+        echo "<div class='innerinfo'>";
+        echo $product["productname"] . "<br>";
+        echo "</div> " . "<br>";
+        echo "<div class='pricebutton'>";
+        echo "€ " . $product["price"];
+        echo "<br>";
+        echo "<a href='product.php?pid=" . $product["id"] . "&category=gpu'>Details werkt nog niet</a>";
+        echo "<br>";  
+        echo "<input type='submit' class='shopbutton' value='Add to cart'>";
+        echo "</div>";
+        echo "</div>";
+    }
+}
+    echo "</div>";
+?>
+
+</body>
+
+</html>
---
+++ search.php
@@ -0,0 +1,159 @@
+<?php
+session_start();
+include_once("config/connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="js/searchbar.js"></script>
+</head>
+
+<body>
+    <!-- Nav bar -->
+    <nav>
+        <a href="index.php"><img src="images/notchLogo.png" class="notchlogo"></a>
+        <div class="middlenav">
+            <div class="buttons">
+                <p class="selectedtab tabs">COMPONENTS <img src="images/caret-down-solidblack.png" alt="" class="dropdownicon"></p>
+            </div>
+
+
+            <p class="tabs">PERIPHERALS <img src="images/caret-down-solidwhite.png" alt="" class="dropdownicon"></p>
+            <p class="tabs">PC'S AND LAPTOPS <img src="images/caret-down-solidwhite.png" alt="" class="dropdownicon"></p>
+        </div>
+        <div class="endnav">
+            <img class="xicon" src="images/xicon.png" alt="xicon" id="xicon" onclick="searchbarhider()">
+            <div id="divdiv">
+
+                <form class="search2" id="search2" onclick="searchbarshower()" method="get" action="search.php">
+
+                    <input type="text" class="searchinput" id="search" name="searchinput">
+                    <img src="images/blacksearch.png">
+                    <input type="submit" id="submitbutton">
+
+                </form>
+            </div>
+        </div>
+        <img src="images/dots.png" alt="options" class="icon">
+        <img src="images/register.png" class="icon">
+        <img src="images/shoppingCard.png" class="icon">
+        </div>
+
+    </nav>
+    <?php
+    if (isset($_GET["searchinput"])) {
+        // Variables nodig voor werking
+        $search = $_GET["searchinput"];
+        if (isset($_GET["sort"])) {
+
+            if (strpos($_GET["sort"], 'nAsc') !== false) { // als $_GET["sort"] nAsc bevat dan..
+                $columnName = "productname";
+                $sortBy = "asc";
+            } else if (strpos($_GET["sort"], 'nDesc') !== false) {
+                $columnName = "productname";
+                $sortBy = "desc";
+            } else if (strpos($_GET["sort"], 'pDesc') !== false) {
+                $columnName = "price";
+                $sortBy = "desc";
+            } else if (strpos($_GET["sort"], 'pAsc') !== false) {
+                $columnName = "price";
+                $sortBy = "asc";
+            }
+            // sorted search output
+            $_SESSION["search"] = $_GET["searchinput"];
+            $search = $_SESSION["search"];
+            $stmt  = $pdo->prepare("SELECT * FROM products WHERE productname LIKE :pname OR maincategorie LIKE :mcategorie OR subcategorie LIKE :scategorie ORDER BY $columnName $sortBy");
+            $stmt->execute(['pname' => "%$search%", 'mcategorie' => "%$search%", 'scategorie' => "%$search%"]);
+            $data = $stmt->fetchAll();
+
+            // search order manieren
+            echo '<div class="results">';
+            echo count($data) . ' results for "' . $search . '"';
+            echo '</div>';
+            echo '<div class="sortproducts">';
+            if (count($data) != 0) {
+                echo '<a href="search.php?searchinput=' . $search . '&sort=nAsc"><button class ="orderbutton">ABC▲</button></a>
+        <a href="search.php?searchinput=' . $search . '&sort=nDesc"><button class ="orderbutton">ABC▼</button></a>
+        <a href="search.php?searchinput=' . $search . '&sort=pAsc"><button class ="orderbutton">€▲</button></a>
+        <a href="search.php?searchinput=' . $search . '&sort=pDesc"><button class ="orderbutton">€▼</button></a>';
+            }
+            echo '</div>';
+
+            echo "<div class='productplace'>";
+            foreach ($data as $product) {
+                echo "<div class='productinner'>";
+                echo "<div class='imagesize'>";
+                echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>" . "<br>";
+                echo "</div>" . "<br>";
+                echo "<div class='innerinfo'>";
+                echo $product["productname"] . "<br>";
+                echo "</div> " . "<br>";
+                echo "<div class='pricebutton'>";
+                echo "€ " . $product["price"];
+                echo "<br>";
+                echo "<br>";
+                echo "<input type='submit' class='shopbutton' value='Add to cart'>";
+                echo "</div>";
+                echo "</div>";
+            }
+        } else {
+            // Search output zonder toegepast filter
+            $_SESSION["search"] = $_GET["searchinput"];
+            $search = $_SESSION["search"];
+            $stmt  = $pdo->prepare("SELECT * FROM products WHERE productname LIKE :pname OR maincategorie LIKE :mcategorie OR subcategorie LIKE :scategorie");
+            $stmt->execute(['pname' => "%$search%", 'mcategorie' => "%$search%", 'scategorie' => "%$search%"]);
+            $data = $stmt->fetchAll();
+
+            // search order manieren
+            echo '<div class="results">';
+            echo count($data) . ' results for "' . $search . '"';
+            echo '</div>';
+            echo '<div class="sortproducts">';
+            if (count($data) != 0) {
+                if (isset($_GET["sort"])) {
+                }
+                echo '<a href="search.php?searchinput=' . $search . '&sort=nAsc"><button class ="orderbutton">ABC▲</button></a>
+        <a href="search.php?searchinput=' . $search . '&sort=nDesc"><button class ="orderbutton">ABC▼</button></a>
+        <a href="search.php?searchinput=' . $search . '&sort=pAsc"><button class ="orderbutton">€▲</button></a>
+        <a href="search.php?searchinput=' . $search . '&sort=pDesc"><button class ="orderbutton">€▼</button></a>';
+            }
+            echo '</div>';
+            echo "<div class='productplace'>";
+            foreach ($data as $product) {
+                echo "<div class='productinner'>";  
+                echo "<div class='imagesize'>";
+                echo "<img src='/images/" . $product['pictures'] . "' alt='productAfbeelding'" . "class='products'>" . "<br>";
+                echo "</div>" . "<br>";
+                echo "<div class='innerinfo'>";
+                echo $product["productname"] . "<br>";
+                echo "</div> " . "<br>";
+                echo "<div class='pricebutton'>";
+                echo "€ " . $product["price"];
+                echo "<br>";
+                echo "<br>";
+                echo "<input type='submit' class='shopbutton' value='Add to cart'>";
+                echo "</div>";
+                echo "</div>";
+            }
+        }
+    }
+    echo "</div>";
+
+
+    if (empty($data)) {
+        $search = 0;
+        echo '<div class="noitems">';
+        echo '<h1> Oops! Looks like we dont have any results for "' . $search . '"</h1><br>';
+        echo 'Check your spelling or use more general terms.</div>';
+    }
+    ?>
+
+</body>
+
+</html>
