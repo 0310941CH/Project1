accb67f561ce6b25fdb3c3ca87733d4ae9b5ec46
ThomasMulders <0341091@student.rocvantwente.nl>
2022-05-31 09:11:22 +0200

    Register page fix password vertification secure demo ticket

--- register.php
+++ register.php
@@ -23,27 +23,34 @@ include_once('config/connection.php');
         // Checked of de input velden ingevuld zijn zo ja gaat die door het naar de database pushen.
         if ($_POST['voornaam'] != "" && $_POST['achternaam'] != "" && $_POST['username'] != "" && $_POST['password'] != "" && $_POST['confirmpassword'] != "") {
             if ($_POST['password'] == $_POST['confirmpassword']) {
+                // Verifying if password reaches te goals of th secure password.
+                $uppercase = preg_match('@[A-Z]@', $_POST['password']);
+                $lowercase = preg_match('@[a-z]@', $_POST['password']);
+                $number    = preg_match('@[0-9]@', $_POST['password']);
+                $specialChars = preg_match('@[^\w]@', $_POST['password']);
+                if ($uppercase && $lowercase && $number && $specialChars && strlen($password) < 5) {
+                    $firstname = $_POST['voornaam'];
+                    $surname = $_POST['achternaam'];
+                    $username = $_POST['username'];
+                    $passwordCommit = $_POST['password'];
+                    //Password Encryption
+                    $hash = password_hash($passwordCommit, PASSWORD_DEFAULT);
+
+                    // PDO Gedeelte
+                    try {
+                        $query = 'INSERT INTO users (voornaam, achternaam, username, passwords) VALUES (:vname, :aname, :uname, :pswrd)';
+                        $values = [':vname' => $voornaam, ':aname' => $achternaam, ':uname' => $username, ':pswrd' => $hash];
 
 
-                $voornaam = $_POST['voornaam'];
-                $achternaam = $_POST['achternaam'];
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                //Password Encryption
-                $hash = password_hash($password, PASSWORD_DEFAULT);
-
-                // PDO Gedeelte
-                try {
-                    $query = 'INSERT INTO users (voornaam, achternaam, username, passwords) VALUES (:vname, :aname, :uname, :pswrd)';
-                    $values = [':vname' => $voornaam, ':aname' => $achternaam, ':uname' => $username, ':pswrd' => $hash];
-
-
-                    $execute = $pdo->prepare($query);
-                    $execute->execute($values);
-                    header('Location: login.php');
-                    exit();
-                } catch (PDOException $e) {
-                    $output = "This username is already in use!";
+                        $execute = $pdo->prepare($query);
+                        $execute->execute($values);
+                        header('Location: login.php');
+                        exit();
+                    } catch (PDOException $e) {
+                        $output = "This username is already in use!";
+                    }
+                } else {
+                    $output = "Password should be at least 5 characters in length and should include at least one upper case letter, one number, and one special character.";
                 }
             } else {
                 $output = "Passwords don't match!";
