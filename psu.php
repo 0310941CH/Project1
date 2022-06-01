5175b377e990b495e9d5cc3ecb47107cc87ed428
Rens <RensGervink>
2022-06-01 11:51:59 +0200

    alles kan naar de shoppingcart

--- psu.php
+++ psu.php
@@ -65,7 +65,7 @@ include_once("config/connection.php");
             echo "<br>";
             echo "<a class='detailbutton' href='product.php?pid=" . $product["id"] . "'>Details</a>";
             echo "<br>";
-            echo "<a class='shopbutton' href='cart_add.php?id=" . $product["id"] . "&&page=cpu.php'>Add to cart</a>";
+            echo "<a class='shopbutton' href='cart_add.php?id=" . $product["id"] . "&&page=psu.php'>Add to cart</a>";
             echo "</div>";
             echo "</div>";
         }
@@ -99,7 +99,7 @@ include_once("config/connection.php");
             echo "<br>";
             echo "<a class='detailbutton' href='product.php?pid=" . $product["id"] . "'>Details</a>";
             echo "<br>";
-            echo "<a class='shopbutton' href='cart_add.php?id=" . $product["id"] . "&&page=cpu.php'>Add to cart</a>";
+            echo "<a class='shopbutton' href='cart_add.php?id=" . $product["id"] . "&&page=psu.php'>Add to cart</a>";
             echo "</div>";
             echo "</div>";
         }
