901620ddb0e20ade55f238a2286aba86e1af1e2a
ThomasMulders <0341091@student.rocvantwente.nl>
2022-06-08 08:45:34 +0200

    Import.sql gechanged

--- sqlImporten/import.sql
+++ sqlImporten/import.sql
@@ -5,10 +5,10 @@ USE `notch`;
 
 CREATE TABLE `users` (
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    voornaam varchar(100) NOT NULL UNIQUE,
-    achternaam varchar(100) NOT NULL UNIQUE,
+    voornaam varchar(100) NOT NULL,
+    achternaam varchar(100) NOT NULL,
     username char(100) NOT NULL UNIQUE,
-    passwords varchar(255) NOT NULL UNIQUE,
+    passwords varchar(255) NOT NULL,
     rank integer(10) NOT NULL
 );
 
